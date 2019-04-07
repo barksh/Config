d82dd9326a465dd4ba713796d29bd2313f83e303
WMXPY <wm@mengw.io>
2019-04-06 19:01:20 -0500

    implement windows path

--- src/path.ts
+++ src/path.ts
@@ -13,14 +13,22 @@ export const getAppDataPath = (path: string = '.barksh'): string => {
 
     const os: NodeJS.Platform = OS.platform();
     const home: string = OS.homedir();
+
     switch (os) {
 
         case 'darwin': return Path.join(home, 'Library', 'Application Support', path);
         case 'linux': {
             const linuxPath: string | undefined = process.env.XDG_CONFIG_HOME;
-            return linuxPath || Path.join(home, '.config', path);
+            return linuxPath
+                ? Path.join(linuxPath, path)
+                : Path.join(home, '.config', path);
         }
-        case 'win32': throw new Error('...TODO');
+        case 'win32': {
+            const appDataPath: string | undefined = process.env.APPDATA;
+            return appDataPath
+                ? Path.join(appDataPath, path)
+                : Path.join(home, 'AppData', 'Roaming', path);
+        }
         default: return Path.join(home, path);
     }
 };
